3d314b6a38f72e31c39d9bbf5894e7ca2b2dc110
unknown <rahul25062@gmail.com>
2023-05-04 20:30:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('StopBtn');
+let startBtn = document.getElementById('StartBtn');
+let resetBtn = document.getElementById('ResetBtn');
+
+let msec = 00;
+let secs = 00;
+let mins = 00;
+
+let timerId = null;
+
+startBtn.addEventListener('click', function () {
+    if (timerId !== null) {
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click', function () {
+    clearInterval(timerId);
+
+});
+
+resetBtn.addEventListener('click', function () {
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00:00:00`;
+    // msec=secs=mins=00;
+
+});
+
+function startTimer() {
+    msec++;
+    if (msec == 100) {
+        msec = 0;
+        secs++;
+        if (secs == 60) {
+            secs = 0;
+            mins++;
+        }
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+
+    // if(msec<10){
+    //     msecString =`0${msec}`;
+    // }else{
+    //     msecString=msec;
+    // }
+    timerDisplay.innerHTML = `${minsString} :${secsString} :${msecString}`;
+}
